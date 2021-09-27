e34ec9e432d89c02254b5a49d78f751786a58f6e
Enes Ahmeti <enes.ahmeti@juspay.in>
2021-09-27 22:17:46 +0300

    refactored code

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+title: Physionet-CSV-Conversion
+author: Abhishek Patil, Enes Ahmeti
+date: 27/09/2021
+
+
 # Physionet-CSV-Conversion
 Code to access the ECG data from Physionet using Python (Access ".dat" file). 
 
@@ -6,5 +11,9 @@ Code to access the ECG data from Physionet using Python (Access ".dat" file).
 * Apnea Database
 * MIT-BIH-Arrythmia
 * PTBDB 
+* Drivers Dataset (http://ieeexplore.ieee.org/xpls/abs_all.jsp?arnumber=1438384)
 
+## Dependencies
+* Numpy - <code>pip install numpy</code>
+* wfdb - <code>pip install wfdb</code>
 
--- script.py
+++ script.py
@@ -1,27 +1,30 @@
-"""
-Code to convert all .dat files (ECG signals) in a folder to CSV format 
-@author: Abhishek Patil
-"""
-import wfdb #WaveForm-Database package. A library of tools for reading, writing, and processing WFDB signals and annotations.
-import pandas as pd
-import numpy as np
+import re
 import glob
+import numpy as np
+import wfdb #WaveForm-Database package. A library of tools for reading, writing, and processing WFDB signals and annotations.
+from pathlib import Path
+
+def convert_to_csv(path='*.dat', new_folder=True):
+    """
+    Code to convert all .dat files (ECG signals) in a folder to CSV format
+    Path - location of the dat files (str)
+    new_fodlder - whether to put the files in a new csv folder (boolean)
+
+    @authors: Abhishek Patil, Enes Ahmeti
+    """
+    print('Reading files...')
+    if new_folder:
+        Path("csv_files").mkdir(parents=True, exist_ok=True)
+    dat_files = glob.glob(path) # Get list of all .dat files in the current folder
+    for file in dat_files:
+        print(file)
+        filename = re.match("([^.]*)", file).group() # Get only the name from file.
+        record, fields = wfdb.rdsamp(filename) # Read the data
+        header = ",".join(["-".join(x) for x in zip(fields['sig_name'], fields['units'])]) # column headers
+        filename = f"csv_files/{filename}.csv" if new_folder else f"{filename}.csv" # Add to the corresponding folder 
+        np.savetxt(filename, record, delimiter=",", header=header) # Save text.
+    print('All files read successfully!')
 
 
-dat_files=glob.glob('*.dat') #Get list of all .dat files in the current folder
-df=pd.DataFrame(data=dat_files)
-df.to_csv("files_list.csv",index=False,header=None) #Write the list to a CSV file
-files=pd.read_csv("files_list.csv",header=None)
-
-for i in range(1,len(files)+1):
-	recordname=str(files.iloc[[i]])
-	print(recordname[:-4])
-	recordname_new=recordname[-7:-4] #Extracting just the filename part (will differ from database to database)
-	record = wfdb.rdsamp(recordname_new) # rdsamp() returns the signal as a numpy array  
-	record=np.asarray(record[0])
-	path=recordname_new+".csv"
-	np.savetxt(path,record,delimiter=",") #Writing the CSV for each record
-	print("Files done: %s/%s"% (i,len(files)))
-
-print("\nAll files done!")	
-
+if __name__ == "__main__":
+    convert_to_csv()
